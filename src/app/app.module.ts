0574e5dfccbb3880e9142678415f31600a97f266
shaikhuzma <shaikhu75@gmail.com>
2019-02-25 16:17:56 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FormComponent } from './form/form.component';
+import { HomeComponent } from './home/home.component';
+import { CategoryComponent } from './category/category.component';
+import { FooterComponent } from './footer/footer.component';
+import { StoriesComponent } from './stories/stories.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    FormComponent,
+    HomeComponent,
+    CategoryComponent,
+    FooterComponent,
+    StoriesComponent,
+    ContactComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
